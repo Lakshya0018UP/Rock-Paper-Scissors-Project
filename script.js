d85f1465fba5aa0f6358de33565a1eac10e73965
Lakshya Garg <122142987+Lakshya0018UP@users.noreply.github.com>
2023-09-17 15:33:26 +0530

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-/*
-  Rock Paper Scissors 🚀🔥
-  Concepts covered in this project
-    👉 For loops
-    👉 Dom Manipulation
-    👉 Variables
-    👉 Conditionals (if else if)
-    👉 Template Literals
-    👉 Event Listeners
-    👉 Higher order Function (Math.random())
-*/
+
 
 // ** getComputerChoice randomly selects between `rock` `paper` `scissors` and returns that string **
 // getComputerChoice() 👉 'Rock'
